1aab7e961a000ba5cffd70f1241ef8a6b54f0fa3
ジャッジメント・デイ <2543164964@qq.com>
2024-11-29 19:27:04 +0800

    add index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,52 +2,173 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'My Yii Application';
 ?>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        height: 100vh;
+        margin: 0;
+        overflow: hidden;
+        position: relative;
+    }
+
+    /* 背景图片轮播 */
+    .background-slideshow {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        overflow: hidden;
+    }
+
+    .background-slideshow img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        opacity: 0;
+        transition: opacity 1s ease-in-out;
+        z-index: -1;
+    }
+
+    .background-slideshow img.active {
+        opacity: 1;
+        z-index: 0;
+    }
+
+    .site-index {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        z-index: 2;
+        height: 100%;
+        text-align: center;
+        color: #fff;
+    }
+
+    .jumbotron {
+        background-color: rgba(0, 0, 0, 0.5);
+        padding: 40px;
+        border-radius: 10px;
+        margin-bottom: 30px;
+    }
+
+    .btn-success {
+        background-color: #4e73df;
+        border-color: #4e73df;
+        color: #fff;
+        font-size: 16px;
+        padding: 10px 20px;
+        border-radius: 5px;
+    }
+
+    .btn-success:hover {
+        background-color: #3e63c9;
+        border-color: #3e63c9;
+    }
+
+    .body-content {
+        background-color: rgba(255, 255, 255, 0.8);
+        padding: 20px;
+        border-radius: 10px;
+        max-width: 800px;
+        margin: 0 auto;
+    }
+
+    .row {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .col-lg-4 {
+        flex: 1;
+        margin: 0 10px;
+        background-color: rgba(255, 255, 255, 0.9);
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+    }
+
+    .col-lg-4 h2 {
+        color: #4e73df;
+    }
+
+    .col-lg-4 p {
+        color: #333;
+    }
+
+    .btn-default {
+        background-color: #4e73df;
+        color: #fff;
+        border: none;
+        padding: 10px 15px;
+        border-radius: 5px;
+    }
+
+    .btn-default:hover {
+        background-color: #3e63c9;
+    }
+</style>
+
 <div class="site-index">
-
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
+        <h1><?= Html::encode($this->title) ?></h1>
         <p class="lead">You have successfully created your Yii-powered application.</p>
-
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
-
         <div class="row">
             <div class="col-lg-4">
                 <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
-
     </div>
 </div>
+
+<!-- 背景图片轮播 -->
+<div class="background-slideshow">
+    <img src="<?= Yii::getAlias('@web') ?>/assets/images/背景图片1.jpg" class="active">
+    <img src="<?= Yii::getAlias('@web') ?>/assets/images/背景图片2.jpg">
+    <img src="<?= Yii::getAlias('@web') ?>/assets/images/背景图片3.jpg">
+</div>
+
+<!-- 图片轮播 JavaScript -->
+<script>
+    let currentImageIndex = 0;
+    const images = document.querySelectorAll('.background-slideshow img');
+    const totalImages = images.length;
+
+    setInterval(() => {
+        images[currentImageIndex].classList.remove('active');
+        currentImageIndex = (currentImageIndex + 1) % totalImages;
+        images[currentImageIndex].classList.add('active');
+    }, 5000);
+</script>
